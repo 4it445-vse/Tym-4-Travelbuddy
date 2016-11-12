290ff1797d0fd5377aa7dbca3bd1d0fc14905bdf
Josef Doskar <xdosj28@vse.cz>
2016-11-12 15:53:26 +0100

    Navbar responsive repair

--- frontend/src/components/Modals/Menu.js
+++ frontend/src/components/Modals/Menu.js
@@ -10,7 +10,7 @@ export default class Menu extends Component {
         this.state = {
             collapsed: true
 		};
-		
+
 		this.toggleNavbar = this.toggleNavbar.bind(this);
 		this.logOut = this.logOut.bind(this);
 	}
@@ -26,32 +26,36 @@ export default class Menu extends Component {
         collapsed: !this.state.collapsed
       });
     }
-	
+
     render() {
 		const {openEdit, openRegister, openLogin} = this.props;
         const loggedUser = currentUser.getCurrentUser();
         const userLogged = !! loggedUser;
         return (
-			<Navbar className="bg-primary" dark>
-			  <NavbarBrand href="/">Travel Buddy</NavbarBrand>
-			  <NavbarToggler className="float-sm-right float-lg-right hidden-lg-up collapsed" onClick={this.toggleNavbar} />
-			  <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
-				<Nav navbar className="float-lg-right">
-				  {userLogged ? <NavItem className="nav-item">
-					  <Link href="#" className="nav-link" onClick={openEdit}>Editovat profil</Link>
-				  </NavItem>  : ""}
-				  {userLogged ? <NavItem className="nav-item">
-					  <Link href="#" className="nav-link" onClick={this.logOut}>Odhlaš se</Link>
-				  </NavItem> : ""}
-				  {userLogged ? "" : <NavItem className="nav-item">
-					  <Link href="#" className="nav-link" onClick={openRegister}>Registrovat se</Link>
-				  </NavItem>}
-				  {userLogged ? "" : <NavItem className="nav-item">
-					  <Link href="#" className="nav-link" onClick={openLogin}>Přihlásit se</Link>
-				  </NavItem>}
-				</Nav>
-			  </Collapse>
-			</Navbar>
+				<Navbar className="bg-primary row" dark>
+						<div className="col-xs-7 col-md-3 text-xs-left">
+							<NavbarBrand href="/">Travel Buddy</NavbarBrand>
+						</div>
+						<div className="col-xs-5 col-md-9 text-xs-right">
+							<NavbarToggler className="hidden-lg-up collapsed" onClick={this.toggleNavbar} />
+						</div>
+				  <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
+					<Nav navbar className="float-lg-right text-xs-center">
+					  {userLogged ? <NavItem>
+						  <Link href="#" className="nav-link" onClick={openEdit}>Editovat profil</Link>
+					  </NavItem>  : ""}
+					  {userLogged ? <NavItem>
+						  <Link href="#" className="nav-link" onClick={this.logOut}>Odhlaš se</Link>
+					  </NavItem> : ""}
+					  {userLogged ? "" : <NavItem>
+						  <Link href="#" className="nav-link" onClick={openRegister}>Registrovat se</Link>
+					  </NavItem>}
+					  {userLogged ? "" : <NavItem>
+						  <Link href="#" className="nav-link" onClick={openLogin}>Přihlásit se</Link>
+					  </NavItem>}
+					</Nav>
+				  </Collapse>
+				</Navbar>
         );
     }
 }
