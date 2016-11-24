36517d855f0e80ba0209a8a814b048f8386c0629
Josef Draslar <j.draslar@gmail.com>
2016-11-24 18:07:04 +0100

    not push!!

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -18,6 +18,61 @@ export class HomePage extends Component {
     }
 
     componentDidMount() {
+	axios.post('messages', {
+	'text':'Hi, this is sample message.',
+	'displayed':false,
+	'date_time': new Date(),
+	'buddy_id_from': 1,
+	'buddy_id_to': 2
+	}).then(response => {console.log('### Message successfully sent.');});
+	axios.post('messages', {
+	'text':'Ahoj, tato správa je zkušební.',
+	'displayed':true,
+	'date_time': new Date(),
+	'buddy_id_from': 3,
+	'buddy_id_to': 881
+	}).then(response => {console.log('### 2nd message successfully sent, but should not!!!');}, error => {console.error('Attemp to insert message with faulty FK!');});
+	axios.get('messages', {
+	params:{
+	filter:{
+	where:{
+	buddy_id_to:1
+	}
+	}
+	}
+	}).then(response => {console.log("incoming messages:");console.log(response);});
+	axios.get('messages', {
+	params:{
+	filter:{
+	where:{
+	buddy_id_from:1
+	}
+	}
+	}
+	}).then(response => {console.log('sent messages:');console.log(response);});
+	axios.post('requests', {
+	'text':'Nechtěl by mě někdo provést Prahou, jsem uživatel 1?',
+	'city':'Praha',
+	'from': new Date(),
+	'to': new Date(),
+	'buddy_id':1
+	}).then(response => {console.log('request sent');});
+	axios.post('requests', {
+	'text':'Jedu do Liberce někdo něco?',
+	'city':'Liberec',
+	'from': new Date(),
+	'to': new Date(),
+	'buddy_id':175
+	}).then(response => {console.log('request sent, but should not!!!!');}, error => {console.error('Attemp to insert request with faulty FK!');});
+	axios.get('requests', {
+	params:{
+	filter:{
+	where:{
+	'city':'Praha'
+	}
+	}
+	}
+	}).then(response => {console.log(response);});
         this.findAllBuddies();
     }
 
