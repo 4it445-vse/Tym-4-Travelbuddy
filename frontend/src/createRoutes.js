e2c840e8f0c0897c1f4f9359dd98f56c54d8108e
Josef Draslar <josef.draslar@broadridge.com>
2016-11-28 15:57:43 +0100

    messaging fix error

--- frontend/src/createRoutes.js
+++ frontend/src/createRoutes.js
@@ -2,7 +2,7 @@ import React from "react";
 import {IndexRoute, Route} from "react-router";
 import {AppPage} from "./pages/AppPage.js";
 import {HomePage} from "./pages/HomePage.js";
-import {MessagesPage} from "./pages/MessagesPage.js";
+import MessagesPage from "./pages/MessagesPage.js";
 import {RequestsPage} from "./pages/RequestsPage.js";
 import { VerifiedPage } from './pages/VerifiedPage.js';
 import { ResetPassword } from './pages/ResetPassword.js';
