96a05701561aa90e9d513ea3d87c220b9d89b376
Josef Draslar <josef.draslar@broadridge.com>
2016-11-29 14:28:49 +0100

    clear comments

--- frontend/src/pages/MessagesPage.js
+++ frontend/src/pages/MessagesPage.js
@@ -147,83 +147,9 @@ export default class MessagePage extends Component {
 				for(let val of this.state.usersWithMessages){
 					console.log("value in map: ", val);
 				}
+				this.setState(this.state);
             }
         });
-
-
-        /*
-         console.log('In MessgePage.findUsers about to querry all verified users.');
-         axios.get('buddies', {
-         params: {
-         filter: {
-         where: {
-         emailVerified: 1
-         }
-         }
-         }
-         }).then(response => {
-         console.log("All verified buddies:", response.data);
-         response.data.map(buddy =>
-         {
-         console.log("About to count messages for buddy with id: ", buddy.id);
-         this.state.currentBuddyInternal = buddy;
-         axios.get('messages', {
-         params: {
-         filter: {
-         where: {
-         or: [
-         {"buddy_id_to": buddy.id},
-         {"buddy_id_from": buddy.id}
-         ]
-         }
-         }
-         }
-         }).then(response => {
-         let buddyMessages = response.data;
-         if(buddyMessages && buddyMessages[0]){
-         console.log("Buddy with id: "+buddyMessages[0].buddy_id_to+" has "+buddyMessages.size+" messages.");
-         let unreadIncomingMessagesNum = 0;
-         let messagesWithCurrentUserNum = 0;
-         let currentUserId = currentUser.getCurrentUser().id;
-         let lastMessageTime = undefined;
-         buddyMessages.map(message =>{
-         if(message.buddy_id_to === currentUserId || message.buddy_id_from === currentUserId){
-         messagesWithCurrentUserNum++;
-         if(lastMessageTime === undefined){
-         lastMessageTime = message.date_time;
-         } else if(lastMessageTime && lastMessageTime < message.date_time){
-         lastMessageTime = message.date_time;
-         }
-         }
-         if(message.buddy_id_to === currentUserId && message.displayed === false){
-         unreadIncomingMessagesNum++;
-         if(lastMessageTime === undefined){
-         lastMessageTime = message.date_time;
-         } else if(lastMessageTime && lastMessageTime < message.date_time){
-         lastMessageTime = message.date_time;
-         }
-         }}
-         );
-         console.warn("if not undefined than should remove currentBuddyInternal and use this one, "+buddy);
-         if(messagesWithCurrentUserNum>0){
-         this.state.usersWithMessages.push({
-         id: this.state.currentBuddyInternal.id,
-         name: this.state.currentBuddyInternal.name,
-         surname: this.state.currentBuddyInternal.surname,
-         unreadIncomingMessagesNum: unreadIncomingMessagesNum,
-         lastMessageTime: lastMessageTime
-         });
-         }
-         }
-
-         });
-         }
-         );
-         this.state.usersWithMessages.sort(function(a,b){
-         return new Date(b.lastMessageTime) - new Date(a.lastMessageTime);
-         });
-         this.setState(this.state);
-         });*/
     }
 
     render() {
