c65ae89f8c80df0da12d9970d49050a1819760ef
Josef Draslar <xdraj24@vse.cz>
2016-12-10 22:47:10 +0100

    send message directly

--- frontend/src/components/Search/User.js
+++ frontend/src/components/Search/User.js
@@ -9,6 +9,7 @@ export default class User extends Component {
             buddy: props.buddy
         }
         this.openProfile = this.openProfile.bind(this);
+        this.openContactBuddy = this.openContactBuddy.bind(this);
     }
 
     openProfile() {
@@ -20,35 +21,44 @@ export default class User extends Component {
         }
     }
 
+    openContactBuddy() {
+        currentUser.openContactBuddy(this.state.buddy);
+    }
+
     render() {
         return (
             <a href="#" onClick={this.openProfile} className="profil_vypis">
                 <div className="card-block">
                     <div className="col-md-1 col-xs-3 no-margin no-padding">
-                        <img src="http://images.megaupload.cz/mystery-man.png" alt={this.state.buddy.name + " " + this.state.buddy.surname} className="profil_img rounded"/>
+                        <img src="http://images.megaupload.cz/mystery-man.png"
+                             alt={this.state.buddy.name + " " + this.state.buddy.surname}
+                             className="profil_img rounded"/>
                     </div>
                     <div className="col-md-3 col-xs-5">
-                      <div className="row">
-                        <p className="no-margin ellipsis">{this.state.buddy.name + " " + this.state.buddy.surname}</p>
-                      </div>
-                      <div className="row">
-                        <span className="no-margin ellipsis">{this.state.buddy.city}</span>
-                      </div>
+                        <div className="row">
+                            <p className="no-margin ellipsis">{this.state.buddy.name + " " + this.state.buddy.surname}</p>
+                        </div>
+                        <div className="row">
+                            <span className="no-margin ellipsis">{this.state.buddy.city}</span>
+                        </div>
                     </div>
                     <div className="col-md-1 col-xs-2">
-                    {
-                      this.state.buddy.sex === 'male' ?
-                      <FontAwesome className="sexIcon" name="male" size="2x" style={{ color: '#0275d8' }}></FontAwesome> :
-                      <FontAwesome className="sexIcon" name="female" size="2x" style={{ color: 'red' }}></FontAwesome>
-                    }
+                        {
+                            this.state.buddy.sex === 'male' ?
+                                <FontAwesome className="sexIcon" name="male" size="2x"
+                                             style={{color: '#0275d8'}}></FontAwesome> :
+                                <FontAwesome className="sexIcon" name="female" size="2x"
+                                             style={{color: 'red'}}></FontAwesome>
+                        }
                     </div>
                     <div className="col-md-6 hidden-sm-down">
-                      <p className="no-margin ellipsis2">{this.state.buddy.about_me}</p>
+                        <p className="no-margin ellipsis2">{this.state.buddy.about_me}</p>
                     </div>
                     <div className="col-md-1 col-xs-2">
-                      <a href="/messages" className="profil_vypis">
-                        <FontAwesome className="sexIcon" name="envelope" size="2x" style={{ color: '#0275d8' }}></FontAwesome>
-                      </a>
+                        <a href="#" onClick={this.openContactBuddy} className="profil_vypis">
+                            <FontAwesome className="sexIcon" name="envelope" size="2x"
+                                         style={{color: '#0275d8'}}></FontAwesome>
+                        </a>
                     </div>
                 </div>
             </a>
